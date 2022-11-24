8541962beeea579571fa4e94d39c100d5e4cbf83
Yuma <yuma08292001@keio.jp>
2022-11-24 09:38:33 +0900

    学習サンプルをランダムに生成した

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
   # x, f(x)の準備
   x= np.linspace(-1,1,101)
   y= np.sin(np.pi * x)##m
+  #sample
+  x_sample = np.random.uniform(-1,1,(20, ))
+  range_y = np.max(y) - np.min(y)
+  noise_sample = np.random.normal(0, range_y*0.05, (20,))
+  y_sample = np.sin(np.pi * x_sample) + noise_sample
   #グラフの作成
   fig = Figure()
   ax = fig.add_subplot(1, 1, 1)
@@ -14,6 +19,7 @@ def main():
   ax.plot(x,y)
   ax.axhline(color='#777777')
   ax.axvline(color='#777777')
+  ax.scatter(x_sample, y_sample, color='red')
   fig.savefig('out.png')
 
 if __name__ == '__main__':
