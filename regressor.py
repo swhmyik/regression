9413cf43ce211bf7b7be13f88d8bed47b0356ee0
Yuma <yuma08292001@keio.jp>
2022-12-08 12:14:01 +0900

    GPRegressorの実装

--- regressor.py
+++ regressor.py
@@ -15,11 +15,29 @@ class PolyRegressor:
   def predict(self,x):
     y_pred = np.squeeze((x[:, np.newaxis] ** self.p) @ self.a)
     return y_pred
+class GPRegressor:
+  def __init__(self, sigma_x,sigma_y):
+    self.sigma_x = sigma_x
+    self.sigma_y = sigma_y
+  def fit(self,x_sample,y_sample):
+    x_s = x_sample[:, np.newaxis]
+    y_s = y_sample[:, np.newaxis]
+    self.x_s = x_s
+    G = self._gaussian(x_s, x_s.T)
+    sigma_I = self.sigma_y *np.eye(G.shape[0])
+    self.a = np.linalg.inv(G + sigma_I)
+  
+  def predict(self,x):
+    g = self._gaussian(x[:,np.newaxis],self.x_s.T)
+
+  def _gaussian(self, col:np.ndarray, row:np.ndarray):
+    return np.exp(- (col - row)**2/(2*(self.sigma_x ** 2)))
 
 def build_regressor(regressor_name,regressor_kwargs):
-  REGRESSORS = dict(
+    REGRESSORS = dict(
       poly = PolyRegressor,
-  )
-  regressor_cls=REGRESSORS[regressor_name]
-  init_kwargs = regressor_kwargs[regressor_name]
-  return regressor_cls(**init_kwargs)
+      gp = GPRegressor,
+   )
+   regressor_cls = REGRESSORS[regressor_name]
+   init_kwargs = regressor_kwargs[regressor_name]
+   return regressor_cls(**init_kwargs)
